0838908530903584e20a5cca6ad5a7e106d55a52
Khoa Vu <kelvinvu.2101@gmail.com>
2025-12-07 21:19:16 -0800

    Use relative metrics path for CI

--- src/metrics.py
+++ src/metrics.py
@@ -1,6 +1,6 @@
 import json, os
 
-METRICS_JSON_PATH = os.getenv("METRICS_JSON_PATH", "/app/data/output/metrics.json")
+METRICS_JSON_PATH = os.getenv("METRICS_JSON_PATH", "data/output/metrics.json")
 
 def export_metrics(metrics: dict):
     os.makedirs(os.path.dirname(METRICS_JSON_PATH), exist_ok=True)
